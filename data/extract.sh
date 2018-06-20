379005c5625314939f9b3218ae6a842c8851d9df
Naomi Shapiro <tsnaomi@uw.edu>
2018-06-20 00:42:23 -0700

    delete extract.sh

--- data/extract.sh
+++
@@ -1,4 +0,0 @@
-time ( python extract.py > 'fi.data' 2> 'fi.errors' )
-# time ( python extract.py -l af > 'af.data' 2> 'af.errors' )
-# time ( python extract.py -l de > 'de.data' 2> 'de.errors' )
-# time ( python extract.py -l en > 'en.data' 2> 'en.errors' )
